eb2b89dc8a04ecec4b5f696fb0547cb86997a717
Christofferos <kristopher.werlinder@telia.com>
2022-02-22 21:44:22 +0100

    Setting up ground work for Spotify Boost

---
+++ pages/index.tsx
@@ -0,0 +1,24 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>Spotify Boost</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>Spotify Boost</h1>
+      <main>
+        {/* Sidebar */}
+        {/* Center */}
+      </main>
+
+      {/* Player */}
+    </div>
+  )
+}
+
+export default Home
