8e98fbb44766c50dc0b4431f3725b32b5837bfca
Kristopher Werlinder <42782387+Christofferos@users.noreply.github.com>
2022-02-24 22:36:13 +0100

    Next.js middleware for authentication implemented!

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,22 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { Sidebar } from '../components/Sidebar'
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <div className="h-screen overflow-hidden bg-black">
       <Head>
         <title>Spotify Boost</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <h1>Spotify Boost</h1>
       <main>
-        {/* Sidebar */}
+        <Sidebar />
         {/* Center */}
       </main>
 
-      {/* Player */}
+      <div>{/* Player */}</div>
     </div>
   )
 }
